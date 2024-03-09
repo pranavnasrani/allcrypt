cd1d4cff1b59b0ad94eef92cb5f607f7da7fc0c1
Pranav <85871566+pranavnasrani@users.noreply.github.com>
2024-03-09 20:01:52 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='allcrypt',
-    version='1.5.0',  # Update with your version number
+    version='1.5.1',  # Update with your version number
     long_description_content_type="text/markdown",
     long_description=open("allcrypt/README.md").read(),
     packages=find_packages(),
